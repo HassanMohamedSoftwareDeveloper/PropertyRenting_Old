f46563adae6af4fbe8de6cc42e1af13c36fcee40
Hassan Mohamed <hmohammed.c@tetco.sa>
2023-11-01 00:40:33 +0200

    enhance reports

--- PropertyRenting.Api/wwwroot/App_Data/Reports/CurrentUnits.sql
+++ PropertyRenting.Api/wwwroot/App_Data/Reports/CurrentUnits.sql
@@ -1,22 +1,22 @@
 ﻿SELECT
-	UnitNumber
+    UnitNumber
    ,UnitName
    ,Building.Name AS 'BuildingName'
    ,Unit.AddressAR
    ,Unit.AddressEN
-   ,CONCAT(District.NameAR, ' - ', District.NameEN) AS 'District'
-   ,CONCAT(City.NameAR, ' - ', City.NameEN) AS 'City'
-   ,CONCAT(Country.NameAR, ' - ', Country.NameEN) AS 'Country'
+   ,District.NameAR AS 'District'
+   ,City.NameAR AS 'City'
+   ,Country.NameAR AS 'Country'
    ,Unit.AnnualRentAmount
    ,Unit.TotalArea
    ,Unit.RentableArea
 FROM Unit
 JOIN Building
-	ON Unit.BuildingId = Building.Id
+    ON Unit.BuildingId = Building.Id
 JOIN District
-	ON Unit.DistrictId = District.Id
+    ON Unit.DistrictId = District.Id
 JOIN City
-	ON District.CityId = City.Id
+    ON District.CityId = City.Id
 JOIN Country
-	ON City.CountryId = Country.Id
+    ON City.CountryId = Country.Id
 ORDER BY BuildingName, Unit.CreatedOnUtc
